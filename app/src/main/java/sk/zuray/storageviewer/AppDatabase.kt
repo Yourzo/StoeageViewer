f3b77e336d1d1dc7a974f1b1529116f96065d13f
Yourzo <jurajkobenovy@gmail.com>
2024-12-21 10:37:09 +0100

    Database added

--- app/src/main/java/sk/zuray/storageviewer/AppDatabase.kt
+++ app/src/main/java/sk/zuray/storageviewer/AppDatabase.kt
@@ -1,4 +1,16 @@
 package sk.zuray.storageviewer
 
-class AppDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+
+@Database(
+    entities = [Customer::class, Product::class, CustomersProducts::class],
+    version = 1,
+)
+abstract class AppDatabase: RoomDatabase() {
+
+    abstract val daoCustomer: CustomerDao
+    abstract val daoProduct: ProductDao
+    abstract val daoCustomersProductDao: CustomersProductsDao
 }
